3c25ba9933ef763c222047829d6b94e8a2e96bc2
Altynai Raiimkulova <p0mogitemnevoiti@gmail.com>
2020-10-04 19:38:59 +0600

    Initial commit

---
+++ app/src/main/java/com/example/counter/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.counter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private var number = 1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            number++
+            displayNumber(number)
+        }
+        displayNumber(number)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt("name", number)
+        val count: TextView = findViewById(R.id.number)
+        outState.putString("number", count.text.toString())
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        number = savedInstanceState.getInt("name")
+        val text = savedInstanceState.getString("text")
+        val count:TextView = findViewById(R.id.number)
+        count.text = text
+    }
+
+    private fun displayNumber(number: Int) {
+        val count:TextView = findViewById(R.id.number)
+        count.text = (String.format("%04d", number))
+    }
+}
